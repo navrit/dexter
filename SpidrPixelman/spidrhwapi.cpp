b53bc06b135022566ac8db946d8841055c31687b
Henk Boterenbrood <boterenbrood@nikhef.nl>
2013-07-22 10:45:31 +0000

    bugfix in spidrReadMatrix()
    git-svn-id: https://svn.cern.ch/reps/SPIDR/software/trunk@1019 4525493e-7705-40b1-a816-d608a930855b

--- SpidrPixelman/spidrhwapi.cpp
+++ SpidrPixelman/spidrhwapi.cpp
@@ -795,8 +795,9 @@ int spidrReadMatrix( int id, u32 *data, u32 sz )
   int chip, size_in_bytes;
   for( chip=0; chip<chips_todo; ++chip )
     {
-      int *frame_data = spidrdaq->frameData( spidrinfo->chipMap[chip],
-					     &size_in_bytes );
+      // NB: SpidrDaq orders devices in 'mapped' fashion, so don't use the map
+      //int *frame_data = spidrdaq->frameData( spidrinfo->chipMap[chip], NO!
+      int *frame_data = spidrdaq->frameData( chip, &size_in_bytes );
       memcpy( (void *) data, (void *) frame_data, size_in_bytes );
       data += MPX_PIXELS;
     }
