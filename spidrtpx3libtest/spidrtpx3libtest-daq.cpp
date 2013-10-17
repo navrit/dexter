4b7a3a5c7adbfe78a5d9812642897be6e86deea6
Henk Boterenbrood <boterenbrood@nikhef.nl>
2013-10-17 17:26:38 +0000

    git-svn-id: https://svn.cern.ch/reps/SPIDR/software/trunk@1879 4525493e-7705-40b1-a816-d608a930855b

---
+++ spidrtpx3libtest/spidrtpx3libtest-daq.cpp
@@ -0,0 +1,136 @@
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+#include "SpidrController.h"
+#include "SpidrDaq.h"
+#include "tpx3defs.h"
+
+int main( int argc, char *argv[] )
+{
+  // ----------------------------------------------------------
+  // Open a control connection to SPIDR-TPX3 module
+  // with address 192.168.100.10, default port number 50000
+  SpidrController spidrctrl( 192, 168, 100, 10 );
+
+  // Are we connected to the SPIDR-TPX3 module?
+  if( !spidrctrl.isConnected() ) {
+    cout << spidrctrl.ipAddressString() << ": "
+	 << spidrctrl.connectionStateString() << ", "
+	 << spidrctrl.connectionErrString() << endl;
+    return 1;
+  }
+
+  //SpidrDaq spidrdaq( 192, 168, 100, 1, 8192 );
+  SpidrDaq spidrdaq( &spidrctrl );
+  string errstr = spidrdaq.errorString();
+  if( !errstr.empty() ) cout << "### SpidrDaq: " << errstr << endl;
+
+  int device_nr = 0;
+
+  // ----------------------------------------------------------
+  // DACs configuration
+
+  if( !spidrctrl.setDacsDflt( device_nr ) )
+    cout << "###setDacsDflt: " << spidrctrl.errorString() << endl;
+
+  // ----------------------------------------------------------
+  // Pixel configuration
+
+  if( !spidrctrl.resetPixels( device_nr ) )
+    cout << "###resetPixels: " << spidrctrl.errorString() << endl;
+
+  // Enable test-bit in all pixels
+  spidrctrl.resetPixelConfig();
+  spidrctrl.configPixel( ALL_PIXELS, ALL_PIXELS, 0, true );
+  if( !spidrctrl.setPixelConfig( device_nr ) )
+    cout << "###setPixelConfig: " << spidrctrl.errorString() << endl;
+
+  // ----------------------------------------------------------
+  // Test pulse and CTPR configuration
+
+  // Timepix3 test pulse configuration
+  if( !spidrctrl.setTpPeriodPhase( device_nr, 10, 0 ) )
+    cout << "###setTpPeriodPhase: " << spidrctrl.errorString() << endl;
+  if( !spidrctrl.setTpNumber( device_nr, 1 ) )
+    cout << "###setTpNumber: " << spidrctrl.errorString() << endl;
+
+  // Enable test-pulses for some columns
+  int col;
+  for( col=0; col<256; ++col )
+    if( col >= 10 && col < 11 )
+      spidrctrl.configCtpr( device_nr, col, 1 );
+
+  if( !spidrctrl.setCtpr( device_nr ) )
+    cout << "###setCtpr: " << spidrctrl.errorString() << endl;
+
+  // ----------------------------------------------------------
+
+  // SPIDR-TPX3 and Timepix3 timers
+  if( !spidrctrl.restartTimers() )
+    cout << "###restartTimers: " << spidrctrl.errorString() << endl;
+
+  // Set Timepix3 acquisition mode
+  if( !spidrctrl.setGenConfig( device_nr,
+			       TPX3_POLARITY_HPLUS |
+			       TPX3_ACQMODE_TOA_TOT |
+			       TPX3_GRAYCOUNT_ENA |
+			       TPX3_TESTPULSE_ENA |
+			       TPX3_FASTLO_ENA |
+			       TPX3_SELECTTP_DIG_ANALOG ) )
+    cout << "###setGenCfg: " << spidrctrl.errorString() << endl;
+
+  // Start acquisition
+  if( !spidrctrl.datadrivenReadout( device_nr ) )
+    cout << "###ddrivenReadout: " << spidrctrl.errorString() << endl;
+
+  // ----------------------------------------------------------
+
+  // Configure shutter trigger
+  int trig_mode      = 4; //SPIDR_TRIG_AUTO;
+  int trig_period_us = 500000; // 500 ms
+  int trig_freq_hz   = 1;
+  int nr_of_trigs    = 10;
+  if( !spidrctrl.setTriggerConfig( trig_mode, trig_period_us,
+				   trig_freq_hz, nr_of_trigs ) )
+    cout << "###setTriggerConfig: " << spidrctrl.errorString() << endl;
+
+  // Sample frames
+  spidrdaq.setSampling( true );
+  spidrdaq.openFile( "test.dat", true );
+
+  // ----------------------------------------------------------
+  // Get and display frames
+
+  // Start triggers
+  if( !spidrctrl.startAutoTrigger() )
+    cout << "###startAutoTrigger: " << spidrctrl.errorString() << endl;
+
+  int cnt = 0, size, x, y, pixdata, timestamp;
+  char *frame;
+  bool got_frame = true;
+  while( got_frame )
+    {
+      got_frame = spidrdaq.getFrame( 3000 );
+      if( got_frame )
+	{
+	  ++cnt;
+	  frame = spidrdaq.frameData( &size );
+	  cout << "Frame " << cnt << " size=" << size << endl;
+	  while( spidrdaq.nextFramePixel( &x, &y, &pixdata, &timestamp ) )
+	    cout << x << "," << y << ": " << hex << pixdata << dec << endl;
+	}
+      else
+	{
+	  cout << "### Timeout --> finish" << endl;
+	}
+    }
+
+  // ----------------------------------------------------------
+  return 0;
+}
--- spidrtpx3libtest/spidrtpx3libtest.cpp
+++ spidrtpx3libtest/spidrtpx3libtest.cpp
@@ -26,11 +26,10 @@ int main( int argc, char *argv[] )
 
   int device_nr = 0, id;
 
-  /*
   int mdegrees, mvolt, mamp, mwatt;
 
   if( !spidrctrl.getDeviceId( device_nr, &id ) )
-    cout << "###setCfg: " << spidrctrl.errorString() << endl;
+    cout << "###getDevId: " << spidrctrl.errorString() << endl;
   //Sleep( 50 );
   if( !spidrctrl.getRemoteTemp( &mdegrees ) )
     cout << "###getRt: " << spidrctrl.errorString() << endl;
@@ -42,15 +41,17 @@ int main( int argc, char *argv[] )
   else
     cout << "ltemp=" << mdegrees << endl;
   //Sleep( 50 );
+  unsigned int timer_lo, timer_hi;
+  if( !spidrctrl.getTimer( device_nr, &timer_lo, &timer_hi ) )
+    cout << "###getTimer: " << spidrctrl.errorString() << endl;
   if( !spidrctrl.getDeviceId( device_nr, &id ) )
-    cout << "###setCfg: " << spidrctrl.errorString() << endl;
+    cout << "###getDevId: " << spidrctrl.errorString() << endl;
   //Sleep( 50 );
   if( !spidrctrl.getAvdd( &mvolt, &mamp, &mwatt ) )
     cout << "###getAvdd: " << spidrctrl.errorString() << endl;
   //Sleep( 50 );
   if( !spidrctrl.getDvdd( &mvolt, &mamp, &mwatt ) )
     cout << "###getDvdd: " << spidrctrl.errorString() << endl;
-  */
     
   // Set some Timepix3 DACs
   int dac_val = spidrctrl.dacMax( TPX3_VTHRESH_COARSE ) / 4;
@@ -58,15 +59,52 @@ int main( int argc, char *argv[] )
     cout << "###setDac1: " << spidrctrl.errorString() << endl;
   if( !spidrctrl.setDac( device_nr, TPX3_VTHRESH_FINE, 0 ) )
     cout << "###setDac2: " << spidrctrl.errorString() << endl;
+  int code;
+  for( code=1; code<=TPX3_DAC_COUNT_TO_SET; ++code )
+    if( spidrctrl.getDac( device_nr, code, &dac_val ) )
+      cout << "DAC " << code << ": " << dac_val << endl;
+    else
+      cout << "### DAC " << code << endl;
+
+  spidrctrl.resetPixels( device_nr );
 
   // Create and upload a Timepix3 pixel configuration
-  //spidrctrl.resetPixelConfig();          // Reset all values to zero
-  //spidrctrl.maskPixel( 34 );             // Mask pixel column 34
-  //spidrctrl.setPixelConfig( device_nr ); // Upload the pixel configuration
+  spidrctrl.resetPixelConfig();          // Reset all values to zero
+  spidrctrl.maskPixel( 254,2 );          // Mask pixel column 34
+  spidrctrl.setPixelConfig( device_nr ); // Upload the pixel configuration
+
+  unsigned char *pixconf = spidrctrl.pixelConfig();
+  int x, y, cnt=0;
+  if( spidrctrl.getPixelConfig( device_nr ) )
+    {
+      for( y=0; y<256; ++y )
+	for( x=0; x<256; ++x )
+	  if( pixconf[y*256+x] != 0 )
+	    {
+	      printf( "%d,%d: %02X\n", y, x,
+		      (unsigned int) pixconf[y*256+x] );
+	      ++cnt;
+	    }
+      cout << "cnt = " << cnt << endl;
+    }
+  else
+    {
+      cout << "###getPixelConfig: " << spidrctrl.errorString() << endl;
+    }
 
   // Set Timepix3 acquisition mode
-  //if( !spidrctrl.setGenConfig( device_nr, TPX3_ACQMODE_TOA_TOT ) )
-  //  cout << "###setCfg: " << spidrctrl.errorString() << endl;
+  if( !spidrctrl.setGenConfig( device_nr,
+			       TPX3_POLARITY_HPLUS |
+			       TPX3_ACQMODE_TOA_TOT |
+			       TPX3_GRAYCOUNT_ENA |
+			       TPX3_TESTPULSE_ENA |
+			       TPX3_FASTLO_ENA |
+			       TPX3_SELECTTP_DIG_ANALOG ) )
+    cout << "###setGenCfg: " << spidrctrl.errorString() << endl;
+
+  // Start acquisition
+  if( !spidrctrl.datadrivenReadout( device_nr ) )
+    cout << "###ddrivenReadout: " << spidrctrl.errorString() << endl;
 
   return 0;
 }
