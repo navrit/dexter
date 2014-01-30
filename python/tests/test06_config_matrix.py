2775501c461d7bf0bcd0231a0b8c6bdecfde5a85
Szymon Kulis <szymon.kulis@cern.ch>
2014-01-30 08:19:22 +0000

    [python] digital test extended
    git-svn-id: https://svn.cern.ch/reps/SPIDR/software/trunk@2597 4525493e-7705-40b1-a816-d608a930855b

--- python/tests/test06_config_matrix.py
+++ python/tests/test06_config_matrix.py
@@ -3,6 +3,7 @@ import random
 import logging
 import numpy
 import time
+import random
 
 class test06_config_matrix(tpx3_test):
   """Configure / Verify matrix configuration"""
@@ -10,169 +11,112 @@ class test06_config_matrix(tpx3_test):
   def _execute(self,**keywords):
 #    pycallgraph.start_trace()
     self.tpx.resetPixels()
+    rnd = random.Random()
+    self.mkdir(self.fname)
 
-    self.tpx.resetPixelConfig()
-    for x in range(256):
-      for y in range(256):
-        dac=0
-        tb=0
-        mask=0
-        self.tpx.setPixelThreshold(x,y,dac)
-        self.tpx.setPixelTestEna(x,y, tb)
-        self.tpx.setPixelMask(x,y,mask)
-    logging.info("Set1")
-    self.tpx.setPixelConfig()
-    logging.info("Set2")
-    eth_filter,cpu_filter=self.tpx.getHeaderFilter()
-    self.tpx.setHeaderFilter(0xffff,cpu_filter&(~0x0200)) # cpu should not see 0x90 packets
+    rnd.seed(0)
+    for pattern in ['zeros','ones','random',]:
+      logging.info(" ")
+      logging.info("Digital patern being used during the test '%s'"%pattern)
+      self.tpx.resetPixelConfig()
+      stimulus={}
+      for x in range(256):
+        stimulus[x]={}
+        for y in range(256):
+          if pattern=='ones':
+            dac=0x0F
+            tb=1
+            mask=1
+          elif pattern=='random':
+            dac= rnd.randint(0, 0xF)
+            tb=rnd.randint(0, 1)
+            mask=rnd.randint(0, 1)
+          else:
+            dac=0
+            tb=0
+            mask=0 
+          def flip_bits4b(v):
+            return (v&1)<<3 | (v&2)<<1 | (v&4)>>1  | (v&8)>>3
+          config=(tb<<5)+( flip_bits4b(dac)<<1 )+mask
+          
+          stimulus[x][y]={'config':config,'received':0, 'ok':False}
+          self.tpx.setPixelThreshold(x,y,dac)
+          self.tpx.setPixelTestEna(x,y, tb)
+          self.tpx.setPixelMask(x,y,mask)
+      self.tpx.setPixelConfig()
+      eth_filter,cpu_filter=self.tpx.getHeaderFilter()
+      self.tpx.setHeaderFilter(0xffff,cpu_filter&(~0x0200)) # cpu should not see 0x90 packets
 
-    self.tpx.send_byte_array([0x90]+[0x00]*(256/8)) 
-    if 0:
-      print "sleep"
-      time.sleep(50)
-      print "read"
+      self.tpx.send_byte_array([0x90]+[0x00]*(256/8)) 
+      if 0:
+        print "sleep"
+        time.sleep(50)
+        print "read"
 
-    self.tpx.setShutterLen(1)
-    self.tpx.openShutter()
-    self.tpx.sequentialReadout(tokens=1)
-    valid_pixels=0
-    logging.info("Read")
-    data=self.tpx.recv_mask(0x71A0000000000000, 0xFFFF000000000000)
-    valid=numpy.zeros((256,256))
-    for d in data:
-      if d.type==0x9:
-        valid[d.col][d.row]+=1
-        valid_pixels+=1
-            
-    if valid_pixels==256*256:
-      logging.info("All pixels were correcly configured and readout")
-    else:
-      logging.info("Only %d pixels were correcly configured and readout (problem with %d pixels)"%(valid_pixels,256*256-valid_pixels))
-      displayed=0
-      bad=""
-      fn=self.fname+".bad"
-      f=open(fn,"w")
+      self.tpx.setShutterLen(1)
+#    self.tpx.openShutter()
+      self.tpx.sequentialReadout(tokens=120)
+      data=self.tpx.recv_mask(0x71A0000000000000, 0xFFFF000000000000)
+#      valid=numpy.zeros((256,256))
+      
+      for d in data:
+        if d.type==0x9:
+          stimulus[d.col][d.row]['received']+=1
+          if d.config==stimulus[d.col][d.row]['config']: 
+            stimulus[d.col][d.row]['ok']=1
+          else:
+            stimulus[d.col][d.row]['err']=d.config
+
+
+      valid_pixels=0
       for x in range(256):
         for y in range(256):
-          if valid[x][y]==0:
-            f.write("1 ")
-            if displayed<20:
-              bad+="(%d,%d) "%(x,y)
-              displayed+=1
-          else:
-            f.write("0 ")
-        f.write("\n")
-      if displayed==20: bad+="..."
-      logging.info("Bad pixels: %s"%bad)
-      logging.info("Storring bad pixel map to %s"%fn)
-
-
-#        if dcol[d.addr]: 
-#          logging.warning("Multiple packets for double column %d"%(d.addr))
-#        else:
-#          expected=ctpr[2*d.addr+1]*2+ctpr[2*d.addr]
-#          if expected!=d.ctpr:
-#            logging.warning("Wrong CTPR value for double column %d (received:%d, expected:%d)"%(d.addr, d.ctpr, expected))
+          if stimulus[x][y]['received']==1 and stimulus[x][y]['ok']==1:
+            valid_pixels+=1
 #          else:
-#            #looks fine
-#            dcol[d.addr]=1
-#            if not d.toa in toa:
-#              toa[d.toa]=0
-#            toa[d.toa]+=1
-#              
-#          
-#      elif d.type!=0x7:
-#        logging.warning("Unexpected packet %s"%str(d))
-#    err=0
-#    if sum(dcol)!=128:
-#      logging.warning("Received data for only %d double columns (should be 128)"%(sum(dcol)) )
-#      missing=""
-#      for d in range(128):
-#        if dcol[d]==0:
-#           missing+="%d "%d
-#      logging.warning("Missing double columns %s"%missing )
-#        
-#      logging.warning("Received data:")
-#      for d in data:
-#        logging.warning("  %s"%str(d) )
-
-#      data=self.tpx.recv_mask(0x71D0000000000000, 0xFFFF000000000000)
-#      for d in data:
-#        logging.warning("  >> %s"%str(d) )
-#      err=1
-#    if len(toa.keys())>2:
-#      logging.warning("TOA spread to high (values:%s)"%(str(toa.keys())) )
-#      err=1
-#    if not err:
-#      logging.info("CTPR OK")
-    
-#        config=[]
-#        for sub_column in range(column,column+cstep):
-#          logging.debug("LoadMatrixConfig for column %d"%sub_column)
-#          LoadMatrixConsig=0x80
-#          pixel_config=[]
-#          for p in range(256):
-#            pixel_config.append(random.randint(0,0x3F))
-#          config.append(pixel_config)
-#          buf=[LoadMatrixConsig]+gen_col_mask(sub_column)+gen_config_vector(pixel_config)
-#          self.tpx.send_byte_array(buf)
-#          resp=self.tpx.recv_mask(0x8f71,0xFFFF)
-#          if len(resp)>1:
-#            for p in resp:
-#              print p
-
-#        logging.debug("Read Config Matrix")
-#        ReadConfigMatrix=0x90
-#        buf=[ReadConfigMatrix]+gen_col_mask(range(column,column+cstep))
-#        self.tpx.send_byte_array(buf)
-#        resp=self.tpx.recv_mask(0x9f71,0xFFFF)
-#        if len(resp)>1:
-#          for p in resp:
-#            print p
+#            print x,y,stimulus[x][y]['received'],stimulus[x][y]['ok']
 
 
-#        logging.debug("Read sequential")
-#        ReadMatrixSeq=0xA0
-#        buf=[ReadMatrixSeq]+[0]*28+[0x00,0x0,0x1,0x0]
-#        self.tpx.send_byte_array(buf)
-#        resp=self.tpx.recv_mask(0xa071,0xFFFF)
-#        
-#        matrix=dict()
+      fn=self.fname+".bad"
+      if valid_pixels==256*256:
+        logging.info("All pixels were correcly configured and readout")
+      else:
+        logging.info("Only %d pixels were correcly configured and readout (problem with %d pixels)"%(valid_pixels,256*256-valid_pixels))
+        fn=self.fname+"/%s.map"%pattern
+        fmap=open(fn,"w")
+        logging.info("Storring bad pixel map to %s"%fn)
 
-#        errors=0
-##        if resp[-1].type!=7: 
-##          errors=255
-##          self.info( "Timeout !! (%d packets received)"%(len(resp)))
-##        elif len(resp)<256:
-##          errors+=256-len(resp)
-##          self.info( "Too less packets !! (%d packets received)"%(len(resp)))
-#        
-#        for p in resp:
-#          if p.type==0x9:
-#            if not p.col in matrix : 
-#              matrix[p.col]={}
-#            if not p.row in matrix[p.col] : 
-#              matrix[p.col][p.row]=p.config
-#              cc=p.col-column
-#              if p.config!=config[cc][p.row]:
-#                print "  Pixel configuration (%d,%d) failed !!"%(p.col,p.row)
-#                errors+=1
-#            else:
-#              print "  Pixel configuration (%d,%d) received multiple time"%(p.col,p.row)
-#              errors+=1
-#        missing=0
-#        logging.info("Total packets received %d"%len(resp))
-#        for k in matrix:
-#          if len(matrix[k])<256: 
-#            l="%d missing pixels in column %d ( "%(256-len(matrix[k]),k)
-#            for i in range(256):
-#              if i not in matrix[k]: 
-#                l+="%d "%i
-#                missing+=1
-#              
-#            logging.error(l+")")
-#        self._assert_true((missing==0),"Missing pixels %d"%(missing))
-#        self._assert_true((errors==0),"Failing pixels %d"%(errors))
-##    pycallgraph.make_dot_graph('test.png')
+        fn=self.fname+"/%s.details"%pattern
+        fdet=open(fn,"w")
+        logging.info("Storring bad details list to %s"%fn)
+
+        missing=""
+        missing_displayed=0
+
+        bad=""
+        bad_displayed=0
+        for x in range(256):
+          for y in range(256):
+            if stimulus[x][y]['received']<1:
+              fmap.write("1 ")
+              fdet.write("(%3d,%3d) - missing\n"%(x,y))
+              if missing_displayed<20:
+                missing+="(%d,%d) "%(x,y)
+                missing_displayed+=1
+            elif stimulus[x][y]['ok']!=1:
+              fmap.write("1 ")
+              fdet.write("(%3d,%3d) - bad conf %02x insted of %02x\n"%(x,y,stimulus[d.col][d.row]['err'], stimulus[d.col][d.row]['config']))
+              if bad_displayed<20:
+                bad+="(%d,%d %02x!=%02x) "%(x,y, stimulus[d.col][d.row]['config'], stimulus[d.col][d.row]['err'])
+                bad_displayed+=1
+            else:
+              fmap.write("0 ")
+          fmap.write("\n")
+        fmap.close()
+        fdet.close()
+        if bad_displayed==20: bad+="..."
+        if missing_displayed==20: missing+="..."
+        if bad_displayed>0: logging.info("Bad pixels: %s"%bad)
+        if missing_displayed>0: logging.info("Missing pixels: %s"%missing)
 
 
