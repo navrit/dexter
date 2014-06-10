fa6e47ef63a5a7796def499a3773df37c0574c04
Szymon Kulis <szymon.kulis@cern.ch>
2014-06-10 14:03:31 +0000

    [python] spidrman app starter added
    git-svn-id: https://svn.cern.ch/reps/SPIDR/software/trunk@3202 4525493e-7705-40b1-a816-d608a930855b

---
+++ python/ui/spidrman.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import sys
+import time
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+from tpx3gui import MainWindow
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    pixmap = QPixmap("logo.png")
+    splash = QSplashScreen(pixmap)
+    splash.show()
+    app.processEvents()
+    splash.update()
+    splash.showMessage("")
+    time.sleep(0.1)
+    form=MainWindow()
+
+#   for i in range(5):
+#     s="Loaded modules %s"%("."*i)
+#      splash.showMessage(s)
+#      time.sleep(0.4)
+    time.sleep(1.5)
+    form.show()
+    time.sleep(0.2)
+    splash.finish(form)
+    app.exec_()
--- python/ui/tpx3gui.py
+++ python/ui/tpx3gui.py
@@ -580,7 +580,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
-
     form=MainWindow()
+    app.processEvents()
     form.show()
     app.exec_()
