be6cfa5c512cc685265172a30cf1f795fb7b2086
Henk Boterenbrood <boterenbrood@nikhef.nl>
2014-11-03 08:32:02 +0000

    test setting of various Timepix3 readout speeds
    git-svn-id: https://svn.cern.ch/reps/SPIDR/software/trunk@3830 4525493e-7705-40b1-a816-d608a930855b

---
+++ SpidrTpx3Lib/spidrtpx3libtest/spidrtpx3libtest-speed.cpp
@@ -0,0 +1,91 @@
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+#include "SpidrController.h"
+#include "tpx3defs.h"
+
+int main( int argc, char *argv[] )
+{
+  // Open a control connection to SPIDR-TPX3 module
+  // with address 192.168.100.10, default port number 50000
+  SpidrController spidrctrl( 192, 168, 100, 10 );
+
+  // Are we connected to the SPIDR-TPX3 module?
+  if( !spidrctrl.isConnected() ) {
+    cout << spidrctrl.ipAddressString() << ": "
+	 << spidrctrl.connectionStateString() << ", "
+	 << spidrctrl.connectionErrString() << endl;
+    return 1;
+  }
+
+  int device_nr = 0, id;
+  cout << hex;
+
+  if( !spidrctrl.getDeviceId( device_nr, &id ) )
+    cout << "###getDevId: " << spidrctrl.errorString() << endl;
+  else
+    cout << "DeviceID=" << id << endl;
+
+  // --------------------
+
+  //if( !spidrctrl.setTpxPowerEna( true ) )
+  //  cout << "###setTpxPowerEna: " << spidrctrl.errorString() << endl;
+
+  int stat, config;
+  spidrctrl.reset( &stat, -1 );
+  cout << "reset: stat=" << stat << endl;
+
+  if( !spidrctrl.getOutBlockConfig( device_nr, &config ) )
+    cout << "###getOutBlockConfig: " << spidrctrl.errorString() << endl;
+  else
+    cout << "OutConfig=" << config << endl;
+
+  if( !spidrctrl.getPllConfig( device_nr, &config ) )
+    cout << "###getPllConfig: " << spidrctrl.errorString() << endl;
+  else
+    cout << "PllConfig=" << config << endl;
+
+  int i, speed[] = { 640, 80, 160, 320, 40 };
+  int ena_mask, lock_mask, output_mask = 0xC0;
+  for( i=0; i<5; ++i )
+    {
+      cout << endl << endl;
+
+      if( spidrctrl.setReadoutSpeed( device_nr, speed[i] ) )
+	cout << "Speed: " << dec << speed[i] << hex << endl;
+      else
+	cout << "###setReadoutSpeed " << dec << speed[i] << hex << ": "
+	     << spidrctrl.errorString() << endl;
+
+      if( !spidrctrl.getLinkStatus( device_nr, &ena_mask, &lock_mask ) )
+	cout << "###getLinkStatus: " << spidrctrl.errorString() << endl;
+      else
+	cout << "linkstatus = " << ena_mask << ", " << lock_mask << endl;
+
+      if( !spidrctrl.getOutBlockConfig( device_nr, &config ) )
+	cout << "###getOutBlockConfig: " << spidrctrl.errorString() << endl;
+      else
+	cout << "OutConfig=" << config << endl;
+
+      if( !spidrctrl.getPllConfig( device_nr, &config ) )
+	cout << "###getPllConfig: " << spidrctrl.errorString() << endl;
+      else
+	cout << "PllConfig=" << config << endl;
+
+      output_mask |= (1 << i);
+      if( !spidrctrl.setOutputMask( device_nr, output_mask ) )
+	cout << "###setOutputMask: " << spidrctrl.errorString() << endl;
+      else
+	cout << "OutputMask=" << output_mask << endl;
+
+      Sleep( 2000 );
+    }
+
+  return 0;
+}
