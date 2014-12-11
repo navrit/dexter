f47f62b8c670ff98a7cb88f68a69191373dcc83c
tpoikela <tpoikela@nikhef.nl>
2014-12-11 15:49:37 +0000

    Added a class for analyzing the packets.
    git-svn-id: https://svn.cern.ch/reps/SPIDR/software/trunk@4240 4525493e-7705-40b1-a816-d608a930855b

---
+++ python/tuomas/TPX3PacketAnalysis.py
@@ -0,0 +1,16 @@
+
+
+
+class TPX3PacketAnalyzer:
+
+    def __init__(self):
+        self.finished = False
+        pass
+
+
+    def analyze_packets(self, data):
+        pass
+
+
+    def finished(self):
+        return self.finished
