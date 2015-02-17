7dbe85f8a5800a95567dc6126a1f8f149a5771e4
John Idarraga <idarraga@amscins.com>
2015-02-17 16:18:22 +0000

    git-svn-id: https://svn.cern.ch/reps/SPIDR/software/trunk@4410 4525493e-7705-40b1-a816-d608a930855b

---
+++ SpidrMpx3Eq/mpx3gui.h
@@ -0,0 +1,69 @@
+/**
+ * John Idarraga <idarraga@cern.ch>
+ * Nikhef, 2014.
+ */
+
+
+#ifndef MPX3GUI_H
+#define MPX3GUI_H
+
+//#include <QImage>
+#include <QMainWindow>
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "mpx3eq_common.h"
+
+#define __matrix_size_x 256
+#define __matrix_size_y 256
+
+class QCustomPlot;
+class SpidrController;
+class SpidrDaq;
+class DACs;
+class ThlScan;
+class BarChart;
+
+namespace Ui {
+	class Mpx3GUI;
+}
+
+class Mpx3GUI : public QMainWindow {
+	Q_OBJECT
+
+public:
+	explicit Mpx3GUI(QApplication * coreApp, QWidget *parent = 0);
+	~Mpx3GUI();
+
+	void timerEvent( QTimerEvent * );
+	void PrintFraction(int * buffer, int size, int first_last);
+	int GetNPixelsActive(int * buffer, int size, verblev verbose);
+	pair<int, int> XtoXY(int X, int dimX);
+
+private:
+
+	QApplication * _coreApp;
+	Ui::Mpx3GUI * _ui;
+	SpidrController * _spidrcontrol;
+	SpidrDaq * _spidrdaq;
+
+	// Drawing object
+	//QCustomPlot * _customPlot;
+	BarChart * _chart;
+
+	// Object in charge of performing Thl scans
+	ThlScan * _tscan;
+	// DACs
+	DACs * _dacs;
+
+private slots:
+
+	void StartEqualization();
+	void Connect();
+
+};
+
+
+#endif // MPX3GUI_H
