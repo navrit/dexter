3eeab80631a03fcbca203c35a6ccc4bc40686475
John Idarraga <idarraga@amscins.com>
2016-06-27 15:34:34 +0000

    new syncronization on data taking
    git-svn-id: https://svn.cern.ch/reps/SPIDR/software/trunk@6417 4525493e-7705-40b1-a816-d608a930855b

--- SpidrMpx3Eq/dataset.h
+++ SpidrMpx3Eq/dataset.h
@@ -150,8 +150,8 @@ public:
   void clear();//!< Removes all data
   void resize(int nx, int ny){clear(); m_nx = nx; m_ny = ny;}//!< Changes the size of each chip. Also calls clear().
   void setFramesPerLayer(int newFrameCount); //!<Sets the amount of chips. New Chips get initialized with location (0,0) and a LtRTtB orientation.
-  void setLayer(int *data, int threshold);//!<Overwrites a specific layer with the values pointed to by data.
-  void addLayer(int* data, int threshold);//!<Adds the values pointed to by data to the specified layer.
+  unsigned int setLayer(int *data, int threshold);//!<Overwrites a specific layer with the values pointed to by data.
+  unsigned int addLayer(int* data, int threshold);//!<Adds the values pointed to by data to the specified layer.
   unsigned int setFrame(int *frame, int index, int threshold);//!< Overwrites the data of chip index at the specified threshold with the data pointed to by frame.
   void setPixel(int x, int y, int threshold, int val);//!< Set a pixel value for a given threshold (x,y) (assembly coordinates !)
   unsigned int sumFrame(int *frame, int index, int threshold);//!< Adds the data pointed to by frame to the data of chip index at the specified threshold.
