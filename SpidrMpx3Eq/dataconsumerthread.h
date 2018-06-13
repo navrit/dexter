33cc1daced6853e657a7ebd33ab9a0d190f94f41
Navrit Bal <navrit666@gmail.com>
2018-06-13 14:32:09 +0200

    5 - alignas 512. Doesn't make any difference

--- SpidrMpx3Eq/dataconsumerthread.h
+++ SpidrMpx3Eq/dataconsumerthread.h
@@ -33,7 +33,7 @@ public:
 
     QSemaphore * freeFrames;
     QSemaphore * usedFrames;
-    alignas (64) int * buffer = nullptr;
+    alignas (512) int * buffer = nullptr;
     uint descriptor = 0;
     uint readdescriptor = 0;
 
