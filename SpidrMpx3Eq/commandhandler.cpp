90e08915317ed47f758cfbda1d561ab168d61910
Kiavash <k.matin@amscins.com>
2019-02-12 14:40:03 +0100

    Issue #220 solved. (MPX,0000000021,GET,THRESHOLD0CHIP,0) returns the dac value.

--- SpidrMpx3Eq/commandhandler.cpp
+++ SpidrMpx3Eq/commandhandler.cpp
@@ -330,6 +330,7 @@ int CommandHandler::getThreshold(int idx,int chipId,int *val)
 //         QCstmDacs::getInstance()->getDeviceIdSpinBox()->setValue(chipId);
 //         *val = QCstmDacs::getInstance()->GetSpinBoxList()[idx]->value();
         *val = QCstmGLVisualization::getInstance()->getThresholdVector(chipId,idx);
+         *val = Mpx3GUI::getInstance()->getEnergyCalibrator()->calDac(chipId,*val);
         return NO_ERROR;
     }
     return UNKNOWN_ERROR;
