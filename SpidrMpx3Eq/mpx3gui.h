01ad73c5f9107fdfd66f1b6ac0b15cf24c12cd4a
Navrit Bal <navritbal@gmail.com>
2020-09-18 10:10:15 +0200

    v2.2.7 Minor bug fixes

--- SpidrMpx3Eq/mpx3gui.h
+++ SpidrMpx3Eq/mpx3gui.h
@@ -78,7 +78,7 @@ const static int __thresholdScan_page_Id = 9;
 #define LONG_PERIOD_US 100000000
 
 const static QString _softwareName = "Dexter";
-const static QString _softwareVersion = QString("v2.2.5 Motor tweaks NB - "
+const static QString _softwareVersion = QString("v2.2.7 Minor bug fixes NB - "
                                                 + QString(GIT_CURRENT_SHA1));
 
 const static int tcpCommandPort = 6351; //! Diamond - Merlin interface
